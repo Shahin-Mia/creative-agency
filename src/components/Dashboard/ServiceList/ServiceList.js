7aa19d8f78bb13e3a4149058d0cf16fd78ef30b8
Shahin-Mia <shahin.cmt2@gmail.com>
2020-10-16 22:10:27 +0600

    added query

--- src/components/Dashboard/ServiceList/ServiceList.js
+++ src/components/Dashboard/ServiceList/ServiceList.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../App';
 import './ServiceList.css';
 import ServiceListItem from './ServiceListItem';
 
 const ServiceList = ({ userName }) => {
+    // eslint-disable-next-line no-unused-vars
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [orders, setOrders] = useState([]);
     useEffect(() => {
-        fetch('https://rocky-chamber-63741.herokuapp.com/orders')
+        fetch(`https://rocky-chamber-63741.herokuapp.com/orders?email=${loggedInUser.email}`)
             .then(res => res.json())
             .then(data => setOrders(data))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
